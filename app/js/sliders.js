78960a602e9d0fed37058de25462936c09d75f8e
sergey-murzak <murzak301@gmail.com>
2022-10-24 04:32:57 +0600

    edit adaptive

--- app/js/sliders.js
+++ app/js/sliders.js
@@ -72,7 +72,23 @@ const swiper = new Swiper('.talents__swiper', {
             reverseDirection: false,
             delay: 0,
         },
-        loopedSlides: 4
+        loopedSlides: 4,
+        // breakpoints: {
+        //     // when window width is >= 320px
+        //     320: {
+        //       slidesPerView: 1,
+        //     },
+        //     // when window width is >= 480px
+        //     480: {
+        //       slidesPerView: 3,
+        //       direction: 'horizontal'
+        //     },
+        //     // when window width is >= 640px
+        //     991: {
+        //       slidesPerView: 3,
+        //       direction: 'vertical'
+        //     }
+        //   }
     });
     const projects2 = new Swiper('.projects__items-line-2', {
         // Optional parameters
@@ -86,7 +102,22 @@ const swiper = new Swiper('.talents__swiper', {
             reverseDirection: true,
             delay: 0,
         },
-        loopedSlides: 4
+        loopedSlides: 4,
+        // breakpoints: {
+        //     // when window width is >= 320px
+        //     320: {
+        //       slidesPerView: 1,
+        //     },
+        //     // when window width is >= 480px
+        //     480: {
+        //       slidesPerView: 3,
+        //       direction: 'horizontal'
+        //     },
+        //     // when window width is >= 640px
+        //     991: {
+        //       slidesPerView: 3,
+        //     }
+        //   }
     });
     const projects3 = new Swiper('.projects__items-line-3', {
         // Optional parameters
@@ -99,5 +130,102 @@ const swiper = new Swiper('.talents__swiper', {
             reverseDirection: false,
             delay: 0,
         },
-        loopedSlides: 4
+        loopedSlides: 4,
+        // breakpoints: {
+        //     // when window width is >= 320px
+        //     320: {
+        //       slidesPerView: 1,
+        //     },
+        //     // when window width is >= 480px
+        //     480: {
+        //       slidesPerView: 3,
+        //       direction: 'horizontal'
+        //     },
+        //     // when window width is >= 640px
+        //     991: {
+        //       slidesPerView: 3,
+        //     }
+        //   }
+    });
+
+    const projects4 = new Swiper('.projects__items-line-4', {
+        // Optional parameters
+        loop: true,
+        speed: 6000,
+        spaceBetween: 10,
+        slidesPerView:3,
+        allowTouchMove:false,
+        autoplay: {
+            reverseDirection: false,
+            delay: 0,
+        },
+        loopedSlides: 4,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+            },
+            // when window width is >= 640px
+            991: {
+              slidesPerView: 3,
+            }
+          }
+    });
+    const projects5 = new Swiper('.projects__items-line-5', {
+        // Optional parameters
+        loop: true,
+        speed: 5500,
+        spaceBetween: 10,
+        slidesPerView:3,
+        allowTouchMove:false,
+        autoplay: {
+            reverseDirection: true,
+            delay: 0,
+        },
+        loopedSlides: 4,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+            },
+            // when window width is >= 640px
+            991: {
+              slidesPerView: 3,
+            }
+          }
+    });
+    const projects6 = new Swiper('.projects__items-line-6', {
+        // Optional parameters
+        loop: true,
+        speed: 5000,
+        slidesPerView:3,
+        spaceBetween: 10,
+        allowTouchMove:false,
+        autoplay: {
+            reverseDirection: false,
+            delay: 0,
+        },
+        loopedSlides: 4,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+            },
+            // when window width is >= 640px
+            991: {
+              slidesPerView: 3,
+            }
+          }
     });
